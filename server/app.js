4a9cedbf4b43ee210781edc6c49db2b40ff2b1f9
Samuel Erdtman <erdtman@spotify.com>
2022-07-08 17:50:34 +0200

    adding water endpoint

--- server/app.js
+++ server/app.js
@@ -9,10 +9,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.use(express.static('view'));
+app.use('/water', express.text({ type: '*/*' }),require('./water'));
 app.use('/api', require('./api'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 
 const host = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 8080;
@@ -24,7 +27,7 @@ async function start() {
     return console.log('Unable to connect to Mongo');
   }
   console.log('Connected to Mongo')
-    
+
   app.listen(port, function() {
     console.log(`Listening at http://${host}:${port}`);
   });
---
+++ server/water/index.js
@@ -0,0 +1,51 @@
+/*jshint esversion: 6 */
+/*jslint node: true */
+'use strict';
+
+const water = require('../model/Water.js');
+const bodyParser = require('body-parser');
+
+const moment = require('moment-timezone');
+moment.tz.setDefault("Europe/Stockholm");
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+  res.render('water');
+});
+
+router.post('/measurement', async (req, res) => {
+  const waterData = await water.read();
+
+  console.log(req.body);
+  const hexString = req.body.substring(18, 22);
+  console.log(hexString);
+  if (waterData.measurements.length > 144) {
+    waterData.measurements.shift();
+  }
+
+  const value = {
+    raw: hexString,
+    parsed: Number('0x'+hexString)/1000*1.5,
+    time: new Date().getTime()
+  };
+
+  console.log(waterData);
+
+  waterData.measurements.push(value)
+  await water.write(waterData)
+
+  res.send();
+});
+
+router.get('/measurements', async (_, res) => {
+  const waterData = await water.read();
+
+  console.log(waterData);
+
+  res.json(waterData);
+});
+
+
+module.exports = router;
