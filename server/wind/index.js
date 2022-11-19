0dd9a9afd626a6fccadec3b11daafbd4a02b7e75
Samuel Erdtman <erdtman@spotify.com>
2022-11-19 23:46:54 +0100

    added graph

--- server/wind/index.js
+++ server/wind/index.js
@@ -32,7 +32,7 @@ router.post('/count', async (req, res) => {
   try {
     console.log(req.body);
 
-    if(!req.body.count) {
+    if (!req.body.count) {
       throw new Error("missing count parameter");
     }
 
@@ -57,7 +57,7 @@ router.get('/now', async (_, res) => {
   const windDataNow = await wind.read("now");
   console.log(windDataNow);
   res.json({
-    m_per_s: (windDataNow.count*8.75/60/100).toFixed(2),
+    m_per_s: (windDataNow.count * 8.75 / 60 / 100).toFixed(2),
     time: windDataNow.time
   });
 });
@@ -70,14 +70,84 @@ router.get('/period/:id', async (req, res) => {
     throw new Error({ code: 400, message: 'Missing id parameter' });
   }
 
-  const seconds = 60*60;
+  const seconds = 60 * 60;
   const windData = await wind.read(id);
   console.log(windData);
   res.json({
-    m_per_s: (windData.count*8.75/seconds/100).toFixed(2),
+    m_per_s: (windData.count * 8.75 / seconds / 100).toFixed(2),
     time: windData.time
   });
 });
+
+
+async function getWindData(lookback, period) {
+  const id = moment().subtract(lookback, period).format(formats[period]);
+  return await wind.read(id);
+}
+
+const formats = {
+  years: "YYYY",
+  months: "YYYY-MM",
+  days: "YYYY-MM-DD",
+  hours: "YYYY-MM-DD-HH"
+}
+
+const factor = {
+  years: 60 * 60 * 24 * 365, // TODO adjust for leap year
+  months: 60 * 60 * 24 * 30, // TODO fix exact days in month
+  days: 60 * 60 * 24,
+  hours: 60 * 60
+}
+
+router.get('/graph/:period', async (req, res) => {
+  try {
+
+    const period = req.params.period;
+
+    if (!period || !formats[period]) {
+      throw new Error({ code: 400, message: `Unknown period: ${period}` });
+    }
+
+    const seconds = factor[period];
+    const respData = [];
+    const labels = []
+    const dataset = []
+    for (let index = 0; index < 24; index++) {
+      const wind = await getWindData(index, period)
+      console.log(wind);
+      const data = {
+        m_per_s: (wind.count * 8.75 / seconds / 100).toFixed(2),
+        time: wind._id
+      }
+      labels.push(wind._id);
+      dataset.push(wind.count * 8.75 / seconds / 100);
+      respData.push(data);
+    }
+
+
+    console.log({
+      labels:labels,
+      dataset:dataset,
+    });
+
+    const windDataNow = await wind.read("now");
+    console.log(windDataNow);
+
+
+    res.json({
+      latest: {
+        m_per_s: (windDataNow.count * 8.75 / 60 / 100).toFixed(2),
+        time: windDataNow.time
+      },
+      labels:labels,
+      dataset:dataset,
+    });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
 
 
 module.exports = router;
--- src/wind/App.vue
+++ src/wind/App.vue
@@ -10,32 +10,93 @@
           <h5>{{latest.m_per_s}} m/s</h5>
         </div>
       </div>
+      <div class="chart-container" style="position: relative; height:80vh; width:90vw">
+        <canvas ref="canvas"></canvas>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
 
 export default {
   data() {
+    this.chart = null;
     return {
-      latest: {time: "x", value: "y"}
+      latest: {time: "", value: ""},
+      chartData: {
+        labels: [],
+        datasets: [{
+            label: "Nivå",
+            borderColor: "#0909FF",
+            data: [],
+          },
+        ],
+      },
+      chartType: "line",
+      chartOptions: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+          title: {
+            display: false,
+          },
+          tooltip: {
+            enabled: false,
+          },
+          animation: {
+            duration: 0,
+          },
+
+        },
+        elements: {
+            point:{
+              radius: 0
+            }
+          },
+        tension: 0.5,
+
+        responsive: true,
+        maintainAspectRatio: false,
+
+        scales: {
+          x: {
+            display: true
+          },
+          y: {
+            display: true
+          },
+        },
+      },
     };
   },
   async mounted() {
+    var config = {
+      type: this.chartType,
+      data: this.chartData,
+      options: this.chartOptions,
+    };
+    this.chart = new Chart(this.$refs.canvas, config);
 
     this.update();
   },
   methods: {
     async update() {
       try {
-        const response = await axios.get("/wind/now");
-        this.latest = response.data;
+        const response = await axios.get("/wind/graph/hours");
+        console.log(response.data);
+        this.chartData.labels = response.data.labels;
+        this.chartData.datasets[0].data = response.data.dataset
+
+        this.latest = response.data.latest;
       } catch (error) {
         console.log(`error: ${error.message}`);
       }
-      setTimeout(this.update, 10000);
+      setTimeout(this.update, 60000);
     },
   },
 };
